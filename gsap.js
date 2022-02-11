825c74870847d1f480b82a8587f98c6bd28a42ae
Katya Heylen <ezhgurova@gmail.com>
2022-02-11 16:26:31 +0100

    changes before deployment

--- gsap.js
+++ gsap.js
@@ -1,6 +1,6 @@
-gsap.from("h1", {duration: 0.4, opacity: 0.2, repeat: 300, yoyo: true })
+gsap.from("h1", {duration: 0.4, opacity: 0.2, repeat: 600, yoyo: true })
 
-gsap.to("#pawty", {duration: 2, y: -80, scale: 1.3, repeat: 100, yoyo: true});
+gsap.to("#pawty", {duration: 2, y: -80, scale: 1.3, repeat: 300, yoyo: true});
 
 gsap.to("#portrait", {duration: 2, rotate: 360});
 
